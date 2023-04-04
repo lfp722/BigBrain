bbf8f72d24101d5f36ff52fd2bbb7d679163d7a9
Taehwan Kim <z5260003@ad.unsw.edu.au>
2023-04-04 15:17:45 +1000

    Initial app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,22 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import React from 'react';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import Dashboard from './pages/Dashboard';
+import EditGame from './pages/EditGame';
+import EditQuestion from './pages/EditQuestion';
 
 function App () {
   return (
-    <>Let&apos;s go!</>
+    <Router>
+      <Routes>
+        <Route exact path="/login" element={ <Login /> } />
+        <Route exact path="/register" element={ <Register />} />
+        <Route exact path='/dashboard/:token' element={ <Dashboard />} />
+        <Route exact path='/editGame/:token/:quizId' element={ <EditGame />} />
+        <Route exact path='/editQuestion/:token/:questionId/:quizId' element={ <EditQuestion />} />
+      </Routes>
+    </Router>
   );
 }
 
